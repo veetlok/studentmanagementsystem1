1835aee5f0a46b09ba4dbe754b5b187039113498
1niilo <niilo.h.laine@edu.turkuamk.fi>
2026-03-17 14:02:04 +0200

    Tästä se alkaa.

---
+++ main.py
@@ -0,0 +1,34 @@
+# pohja
+
+class Oppilas:
+    def __init__(self, oppilas_id, nimi, ika):
+        self.oppilas_id = oppilas_id
+        self.nimi = nimi
+        self.ika = ika
+        self.kurssit = []
+        self.arvosanat = {}
+
+
+# tietokanta, johon tieto tallennetaan
+oppilas = [] 
+
+
+# Toimintojen Funktiot. 
+def lisaa_oppilas():
+    pass
+
+def nayta_oppilaat():
+    pass
+
+def etsi_oppilas():
+    pass
+
+def anna_arvosanat():
+    pass
+
+def poista_oppilas():
+    pass
+
+
+
+

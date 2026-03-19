9e905c742203e0d416c733221a399ba52f7b8e2e
veetlok <veeti.lokka@gmail.com>
2026-03-19 15:08:27 +0200

    Update nayta_oppilaat.py

--- nayta_oppilaat.py
+++ nayta_oppilaat.py
@@ -2,4 +2,9 @@ from models import Oppilas
 from data import oppilaat
 
 def nayta_oppilaat():
-    input("moi kaikki")
+    if not oppilaat:
+        print("Ei oppilaita.")
+        return
+    
+    for o in oppilaat:
+        print(o.oppilas_id, o.nimi, o.ika)

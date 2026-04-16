9c6ee6a696634678573d861427141a0d8c71ff3e
1niilo <niilo.h.laine@edu.turkuamk.fi>
2026-04-16 11:48:43 +0300

    Oppilaan arvosanojen keskiarvo laskettuna.

--- nayta_arvosana.py
+++ nayta_arvosana.py
@@ -1,4 +1,7 @@
 from data import oppilaat
+import numpy as np
+
+
 
 def nayta_arvosana():
     
@@ -11,8 +14,12 @@ def nayta_arvosana():
 
     for o in oppilaat:
          if o.oppilas_id == oppilas_id:
-              print(f"Löytyi: Nimi: {o.nimi}, Ikä: {o.ika}, Kurssit: {o.arvosanat}")
-              return
+               print(f"Löytyi: Nimi: {o.nimi}, Ikä: {o.ika}, Kurssit: {o.arvosanat}")  
+                           
+               keskiarvo = np.mean(list(o.arvosanat.values()))
+ 
+               print("Oppilaan arvosanojen keskiarvo: " + str(keskiarvo))     
+               return
          
     print("Oppilasta ei löytynyt.")
 
